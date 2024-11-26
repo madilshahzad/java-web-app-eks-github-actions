ceda1dab793bc54ff2d84d802e171da4df9f047a
Adil <53600644+adilshehzad786@users.noreply.github.com>
2024-11-26 09:23:28 +0000

    adding java app

---
+++ javaWeb/src/main/java/com/java-web-app/JavaWebAppApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+package com.java_web_app;
+
+
+@SpringBootApplication
+public class JavaWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebAppApplication.class, args);
+    }
+}
