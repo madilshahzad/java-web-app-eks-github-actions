223fb601da979ffea6c0f7865f981a962c969aeb
Adil <53600644+adilshehzad786@users.noreply.github.com>
2024-11-28 07:49:52 +0000

    delete: remove unused Terraform configuration files and variables for bastion and VPC modules

--- iaC/modules/bastion/eip.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_eip" "bastion_host_eip"{
-  instance = module.ec2_public.id
-  depends_on = [module.ec2_public]
-  tags = {
-    Name = "bastion_host_eip"
-  }
-}
--- iaC/modules/bastion/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_type"{}
-
-variable "key_name"{}
-
-variable "vpc_id"{}
-
-variable "public_subnets"{}
-
-variable "bastion_name"{}
